41152443f14a4c87bad957646f6dbd20c2fdab24
ErasylYE43 <baerasyl09@gmail.com>
2023-10-08 14:05:38 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import logo from './logo.svg';
+import './App.css';
+import { Button } from 'bootstrap';
+
+function App() {
+  return (
+    <div className="body1">
+      <header className="header1">
+        <div className="LOgo">
+            <img src="pngegg.png" alt="logo"/>
+        </div>
+        <div className="NAme">
+          <h1>ErasylShop</h1>
+        </div>
+      </header>
+      <main>
+        <div>
+          <div>
+        <img src="image 7.png"/>
+        <h1>Одежда для мужчин</h1>
+        </div>
+        <div>
+        <img src="image 17.png"/>
+        <h1>20-31 Размер одежды</h1>
+        </div>
+        <div><img src="image 18.png"/>
+        <h1>32-43 Размер одежды</h1></div>
+        <div><img src="image 18.png"/>
+        <h1>32-43 Размер одежды</h1></div>
+       <div> <img src="image 1.png"/>
+        <h1>Одежда для девушек</h1></div>
+        <div><img src="image 19.png"/>
+        <h1>15-30 Размер одежды</h1></div>
+      </main>
+      </div>
+    </div>
+  );
+} 
+
+export default App;
